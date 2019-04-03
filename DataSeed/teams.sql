4b24146f20c85c0ebc00540e689a87f11a1f04d0
Stefania Tsvetkova <nia_flo@abv.bg>
2019-04-03 12:26:41 +0300

    Data seed updated

--- DataSeed/teams.sql
+++ DataSeed/teams.sql
@@ -1,3 +1,4 @@
+USE [Bookmaker];
+
 INSERT INTO [dbo].[Teams]([Name], [Division], [Budget], [IsDeleted])
-	VALUES	('bg', 1, 100, 0),
-			('en', 1, 1000, 0);
+	VALUES	('FC Barcelona', 1, 960000000, 0);
