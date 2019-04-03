ff7a7a1c770be89979d265d24b2c7d3ba16657dc
Yordan Andreev <Dany_andreew@abv.bg>
2019-04-03 22:27:17 +0300

    second unit test

---
+++ Bookmaker/Bookmaker.Tests/PlayerServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Bookmaker.Data;
+using Bookmaker.Data.Models;
+using Bookmaker.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class PlayerServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AddPlayer_Saves_Coach()
+        {
+            //Arrange
+            var playerSet = new Mock<DbSet<Player>>();
+
+            var mockContext = new Mock<BookmakerContext>();
+            mockContext.Setup(m => m.Players).Returns(playerSet.Object);
+
+            var playerService = new PlayerService(mockContext.Object);
+
+            //Act
+            Player player = new Player()
+            {
+                Age = 20,
+                Name = "Name"
+            };
+            playerService.AddPlayer(player);
+
+            //Assert
+            playerSet.Verify(m => m.Add(It.IsAny<Player>()), Times.Once());
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+    }
+}
