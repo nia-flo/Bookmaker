50b8f68466e0956920394854b308a826c1b0f711
Stefania Tsvetkova <nia_flo@abv.bg>
2019-04-05 21:20:09 +0300

    InjuryService - unuseful not implemented method deleted

---
+++ Bookmaker/Bookmaker.Tests/MatchServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using Bookmaker.Data;
+using Bookmaker.Data.Models;
+using Bookmaker.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using Match = Bookmaker.Data.Models.Match;
+
+namespace Tests
+{
+    public class MatchServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AddMatch_Saves_Match()
+        {
+            //Arrange
+            var matchSet = new Mock<DbSet<Match>>();
+
+            var mockContext = new Mock<BookmakerContext>();
+            mockContext.Setup(m => m.Matches).Returns(matchSet.Object);
+
+            var matchService = new MatchService(mockContext.Object);
+
+            //Act
+            Team host = new Team()
+            {
+                Id = 1,
+                Name = "First",
+                Division = 1,
+                Budget = 1000000,
+                Players = new List<Player>
+                {
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player()
+                }
+            };
+
+            Team guest = new Team()
+            {
+                Id = 2,
+                Name = "Second",
+                Division = 1,
+                Budget = 1000000,
+                Players = new List<Player>
+                {
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player()
+                }
+            };
+
+            Match match = new Match()
+            {
+                HostTeam = host,
+                HostId = 1,
+                GuestTeam = guest,
+                GuestId = 2
+            };
+            matchService.AddMatch(match);
+
+            //Assert
+            matchSet.Verify(m => m.Add(It.IsAny<Match>()), Times.Once());
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+    }
+}
---
+++ Bookmaker/Bookmaker.Tests/TeamServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using Bookmaker.Data;
+using Bookmaker.Data.Models;
+using Bookmaker.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class TeamServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AddTeam_Saves_Team()
+        {
+            //Arrange
+            var teamSet = new Mock<DbSet<Team>>();
+
+            var mockContext = new Mock<BookmakerContext>();
+            mockContext.Setup(m => m.Teams).Returns(teamSet.Object);
+
+            var teamService = new TeamService(mockContext.Object);
+
+            //Act
+            Team team = new Team()
+            {
+                Name = "Name",
+                Division = 1,
+                Budget = 1000000
+            };
+            teamService.AddTeam(team);
+
+            //Assert
+            teamSet.Verify(m => m.Add(It.IsAny<Team>()), Times.Once());
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+    }
+}
