cd6d5227d26b4245a2214fda64d39ab278895a06
Stefania Tsvetkova <nia_flo@abv.bg>
2019-04-06 17:16:44 +0300

    podobreniq

--- Bookmaker/Bookmaker/Services/IInjuryService.cs
+++ Bookmaker/Bookmaker/Services/IInjuryService.cs
@@ -3,21 +3,24 @@
 namespace Bookmaker.Services
 {
     /*
-    The IInjuryService class
-    Contains all methods related to the return of injury .
-*/
+        The IInjuryService interface
+        Contains all methods bound to the injury
+    */
     /// <summary>
-    /// The <c>IInjuryService</c> class.
+    /// The <c>IInjuryService</c> interface.
     /// Contains all methods bound to the injury.
     /// </summary>
-
-
     public interface IInjuryService
     {
 
+        // Adds a injury to the DBContext
+        /// <summary>
+        /// Adds a new injury by <paramref name="name"/> to the DBContext.
+        /// </summary>
         /// <returns>
         /// Nothing
         /// </returns>
+        /// <param name="name">A string.</param>
         void AddInjury(string name);
     }
 }
--- Bookmaker/Bookmaker/Services/InjuryService.cs
+++ Bookmaker/Bookmaker/Services/InjuryService.cs
@@ -5,11 +5,11 @@ using Bookmaker.Data.Models;
 namespace Bookmaker.Services
 {
     /*
-    The InjuryService class
-    Contains all methods bound to the injury
-*/
+        The InjuryService class
+        Contains all methods bound to the injury
+    */
     /// <summary>
-    /// The <c>InjuuryService</c> class.
+    /// The <c>InjuryService</c> class.
     /// Contains all methods bound to the injury.
     /// </summary>
     /// <remarks>
@@ -19,30 +19,28 @@ namespace Bookmaker.Services
     {
         private BookmakerContext context;
 
-        // Constructor
-        /// <summary>
-        /// Initializes a new instance of <c>InjuryService</c> with parameter <paramref name="context"/>.
-        /// </summary>
-        /// <param name="context">A BookmakerContext.</param>
-
         //public InjuryService()
         //{
         //    this.context = new BookmakerContext();
         //}
 
+        // Constructor
+        /// <summary>
+        /// Initializes a new instance of <c>InjuryService</c> with parameter <paramref name="context"/>.
+        /// </summary>
+        /// <param name="context">A BookmakerContext.</param>
         public InjuryService(BookmakerContext context)
         {
             this.context = context;
         }
         // Adds a result to the DBContext
         /// <summary>
-        /// Adds <paramref name="result"/> to the DBContext.
+        /// Adds <paramref name="name"/> to the DBContext.
         /// </summary>
         /// <returns>
         /// Nothing
         /// </returns>
-        /// <param name="result">A Result.</param>
-
+        /// <param name="name">A string.</param>
         public void AddInjury(string name)
         {
             context.Injuries.Add(new Injury()
